7f2f032d01279c71174225f3ca2ec3acb6ac00eb
นายพุฒิพงศ์ ซุงสุวรรณ <6510742460@eng.tu>
2022-09-22 12:00:22 +0700

    22_09_2022p2

--- work/test_mid1P1/midterm-7.py
+++ work/test_mid1P1/midterm-7.py
@@ -2,7 +2,10 @@
 hours_intput = int(input("Enter parking hours (>=0): "))
 minutes_input = int(input("Enter parking minutes (0-59): "))
 if hours_intput == 0:
-    fee = 0
+    if minutes_input <= 30:
+        fee = 0
+    else:
+        fee = 20
 elif minutes_input != 0:
     fee = hours_intput*20 + 20
 else:
