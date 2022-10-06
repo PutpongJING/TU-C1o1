005aff9f865764498a1bc263867e95728e5d9076
psoongsu <psoongsu@psoongsus-MacBook-Air.local>
2022-10-06 16:39:08 +0700

    06_10_2022

---
+++ work/ex2_p47.py
@@ -0,0 +1,12 @@
+#6510742460 Putthipong Soongsuwan
+from this import d
+
+
+def print_digit(a_string):
+    for i in a_string:
+        if i.isdigit():
+            print(i, end=' ')
+    print('')
+print_digit("24 hours in 1 day, 7 days in a week.")
+message = 'I met a man with 7 wives. Each wife had 7 sack.'
+print_digit(message)
